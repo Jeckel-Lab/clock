e8213f34735890b4b6b9d174389e68dbcf5323e7
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-04-19 09:47:52 +0200

    :white_check_mark: Add clock tests

---
+++ tests/ClockTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Test\Jeckel\Clock;
+
+use DateTimeImmutable;
+use Jeckel\Clock\Clock;
+use PHPUnit\Framework\TestCase;
+
+final class SystemClockTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testNow()
+    {
+        $now = new DateTimeImmutable('now');
+        $service = new Clock();
+        $systemClock = $service->now();
+        $this->assertGreaterThanOrEqual($now, $systemClock);
+        $this->assertLessThanOrEqual(new DateTimeImmutable('now'), $systemClock);
+    }
+}
